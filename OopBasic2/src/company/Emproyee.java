03646e1d718ef937bc8cac2bc734da7c8daaf4a8
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-11 10:52:28 +0900

    オブジェクト指向修了できたと思う。

--- OopBasic2/src/company/Emproyee.java
+++ OopBasic2/src/company/Emproyee.java
@@ -2,27 +2,34 @@ package company;
 
 public class Emproyee {
 
-    String name;
-    String department;
-    String position;
-    int employeeId;
+    protected final String name;
+    private final Department department;
+    private final String position;
+    private final int employeeId;
     //フィールド
     
                //コンストラクターの名前はクラスと一致させる！
-    public Emproyee(String name, String department, String position, int employeeId) {
+    public Emproyee(String name, Department department, String position, int employeeId) {
         this.name = name;
         this.department = department;
         this.position = position;
         this.employeeId = employeeId;
     }
     //　↑　コンストラクター（インスタンスの初期化を行う）。コンストラクターは戻り値なしなので、void不要
+    //this.はインスタンスを表す。コンストラクターの引数（茶色）との区別
     
+    //オーバーロード：同じクラス内で同じ名前のメソッドやコンストラクターを複数定義すること
     public void report(int times) {
-        System.out.println(times + "回目の報告をします。 役職： " + position + "、名前 ： " + name);
+        System.out.println(times + "回目の報告をします。 役職：" + position + "、名前：" + name);
     }
     
     public void report() {
         report(1);
+    }
+    
+    public void joinMeeting() {
+        department.meeting();
+        System.out.println("→上記の会議に出席します。部署：" + department.getName() + "名前：" + name);
     }
      
 }
--- OopBasic2/src/company/ObjectSample.java
+++ OopBasic2/src/company/ObjectSample.java
@@ -3,10 +3,21 @@ package company;
 public class ObjectSample {
 
     public static void main(String[] args) {
-        var employee = new Emproyee("鈴木", "営業部", "課長", 100);
+        var department = new Department("営業部", "xx", 1000000);
+        var employee = new Emproyee("鈴木", department, "課長", 100);
+        
         
         employee.report();
         employee.report(2);
+        employee.joinMeeting();
+        
+        System.out.println("");
+        
+        var devDepartment = new Department("開発部", "yy", 0);
+        var engineer = new Emproyee("田中", devDepartment, "一般社員", 88);
+        
+        engineer.report();
+        engineer.joinMeeting();
 
     }
 
